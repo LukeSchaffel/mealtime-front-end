8c728c6defc8a6203537bbf5dc1ec062c063678d
LukeSchaffel <lukeschaffel@gmail.com>
2022-03-27 11:28:14 -0400

    picture show works

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -4,25 +4,32 @@ function RecipeCard({recipe, user, handleDeleteRecipe}) {
   return(
     
     <div className="card">
-      <h2>{recipe.creator}</h2>
+      {recipe.picture ?
       <img 
-        src="https://www.google.com/" 
+        src={recipe.picture}
         alt="A delicious meal"
         className="card-img-top" 
       />
+      : null
+      }
       <div className="card-body">
         <h2 className="card-text">{recipe.name}</h2>
         <p className="card-text">Ingredients: {recipe.ingredients} </p>
         {recipe.calories ?
         <p className="card-text">Calories: {recipe.calories}</p>
-        :
-        <p></p>
+        :null
         }
         <p className="card-text">Preparation time</p>
       </div>
       {
         user.profile === recipe.creator?._id ?
           <div className="card-footer">
+             <Link
+              className='btn btn-sm btn-primary'
+              to='/profiles/profile/schedule'
+            >
+              Add to schedule
+            </Link>
             <Link
               className='btn btn-sm btn-warning'
               to='/edit'
@@ -42,25 +49,7 @@ function RecipeCard({recipe, user, handleDeleteRecipe}) {
           <p className="card-text"> {recipe.creator?.name ? recipe.creator?.name : 'Ninja'}'s recipe</p>
         </div>
       }
-      <div className="card-footer">
-            <Link
-              className='btn btn-sm btn-primary'
-              to='/profiles/profile/schedule'
-            >
-              Add to schedule
-            </Link>
-            <Link
-              className='btn btn-sm btn-warning'
-              to='/edit'
-            >
-              Edit
-            </Link>
-            <button
-              className="btn btn-sm btn-danger m-left"
-            >
-              Delete
-            </button>
-          </div>
+      
     </div>
   )
 }
