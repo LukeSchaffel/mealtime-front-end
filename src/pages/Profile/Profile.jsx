07b3d29c2796a0cb02e479b0b2a57130c84f67a0
LukeSchaffel <lukeschaffel@gmail.com>
2022-03-28 11:58:02 -0400

    button shows and hides

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,55 +1,36 @@
 import { Link } from "react-router-dom";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
-import { useState, useEffect } from 'react'
+import { useState,  } from 'react'
 
 const Profile = (props) => {
   const [ showResults, setShowResults ] = useState(false)
-  const onClick = () => setShowResults(true) ? setShowResults(false) : setShowResults(true)
-
-  const Results = (props) => {
-  return <div> {props.recipes.map(recipe => (
-      recipe.creator._id === props.user.profile ? 
-        <RecipeCard
-        key={recipe._id}
-        recipe={recipe}
-        handleDeleteRecipe={props.handleDeleteRecipe}
-        user={props.user} />
-        : null
-    ))}
-        </div>
-  }
-
-
-
-
-
-  const Results2 = (props) => (
-    props.recipes.map(recipe => (
-      <h2>{recipe.name}</h2>
-    ))
-    
-  )
-
-
-
+  const onClick = () => !showResults ? setShowResults(true) : setShowResults(false)
+  
   return ( 
     <>
       <main>
-        <button onClick={onClick}></button>
+        <h1>Welcome {props.user.name}</h1>
+        <h2>{props.user.email}</h2>
+        <button onClick={onClick}>
+          {!showResults ? <h4>My Meals</h4> 
+          :
+          <h4>Hide My Meals</h4>
+          }
+        </button>
         
         {showResults ? 
-        <div> 
-          {props.recipes.map(recipe => (
-            recipe.creator._id === props.user.profile ? 
-            <RecipeCard
-            key={recipe._id}
-            recipe={recipe}
-            handleDeleteRecipe={props.handleDeleteRecipe}
-            user={props.user} />
-            : null
-            ))}
-        </div>
-        :null
+          <div> 
+            {props.recipes.map(recipe => (
+              recipe.creator._id === props.user.profile ? 
+              <RecipeCard
+              key={recipe._id}
+              recipe={recipe}
+              handleDeleteRecipe={props.handleDeleteRecipe}
+              user={props.user} />
+              : null
+              ))}
+          </div>
+          :null
           }
    
       </main>
