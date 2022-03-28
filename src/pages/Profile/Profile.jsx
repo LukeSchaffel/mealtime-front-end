4e7f5e6e6991c81a973d58be3593349b92c6c971
LukeSchaffel <lukeschaffel@gmail.com>
2022-03-28 11:47:51 -0400

    add show meals button

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,25 +1,57 @@
 import { Link } from "react-router-dom";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
+import { useState, useEffect } from 'react'
 
 const Profile = (props) => {
+  const [ showResults, setShowResults ] = useState(false)
+  const onClick = () => setShowResults(true) ? setShowResults(false) : setShowResults(true)
+
+  const Results = (props) => {
+  return <div> {props.recipes.map(recipe => (
+      recipe.creator._id === props.user.profile ? 
+        <RecipeCard
+        key={recipe._id}
+        recipe={recipe}
+        handleDeleteRecipe={props.handleDeleteRecipe}
+        user={props.user} />
+        : null
+    ))}
+        </div>
+  }
+
+
+
+
+
+  const Results2 = (props) => (
+    props.recipes.map(recipe => (
+      <h2>{recipe.name}</h2>
+    ))
+    
+  )
+
+
+
   return ( 
     <>
       <main>
-        <h1>Welcome, {props.user.name}</h1>
-        <h2>{props.user.email}</h2>
-        {props.recipes.map(recipe => (
-         <div>
-           {recipe.creator._id === props.user.profile ? 
-           <RecipeCard
-           key={recipe._id}
-           recipe={recipe}
-           handleDeleteRecipe={props.handleDeleteRecipe}
-           user={props.user} />
-           : null}
-          
-          </div>
-          
-        ))}
+        <button onClick={onClick}></button>
+        
+        {showResults ? 
+        <div> 
+          {props.recipes.map(recipe => (
+            recipe.creator._id === props.user.profile ? 
+            <RecipeCard
+            key={recipe._id}
+            recipe={recipe}
+            handleDeleteRecipe={props.handleDeleteRecipe}
+            user={props.user} />
+            : null
+            ))}
+        </div>
+        :null
+          }
+   
       </main>
 
     </> 
