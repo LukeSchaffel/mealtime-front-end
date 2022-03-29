ad69f6bc4391583c5f10c030f3f3b3a2d4429b7f
LukeSchaffel <lukeschaffel@gmail.com>
2022-03-29 19:15:42 -0400

    style and add main to recipes

--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -5,16 +5,18 @@ import styles from './Recipes.module.css'
 const Recipes = (props) => {
   return ( 
     <>
-    <h1>All Recipes</h1>
-      <div id={styles.recipeDiv}>
-        {props.recipes.map(recipe => (
-          <RecipeCard
-            key={recipe._id}
-            recipe={recipe}
-            handleDeleteRecipe={props.handleDeleteRecipe}
-            user={props.user} />
-        ))}
-      </div>
+    <main>
+      <h1>All Recipes</h1>
+        <div id={styles.recipeDiv}>
+          {props.recipes.map(recipe => (
+            <RecipeCard
+              key={recipe._id}
+              recipe={recipe}
+              handleDeleteRecipe={props.handleDeleteRecipe}
+              user={props.user} />
+          ))}
+        </div>
+    </main> 
     </> 
   );
 }
