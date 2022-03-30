985380b3e2672d743334a9004efcf637d2cb2aec
Illia Brytun <elbrytun@gmail.com>
2022-03-29 22:19:18 -0400

    add service function to add restaurants to recipes

--- src/components/RestaurantCard/RestaurantCard.jsx
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useState } from 'react'
 
 function RestaurantCard({restaurant, user, handleDeleteRestaurant}) {
   const [ loggedIn, setLoggedIn  ] = useState(user ? true : false)
-  
+  let location = useLocation()
+
   return(
     
     <div className="card">
@@ -14,48 +15,53 @@ function RestaurantCard({restaurant, user, handleDeleteRestaurant}) {
       />
       <div className="card-body">
         <h2 className="card-text">
-          <Link
-            to={`/restaurants/${restaurant._id}`}
-          >
-            {restaurant.name}
-          </Link></h2>
+          {restaurant.name}
+        </h2>
         <p className="card-text">location: {restaurant.location} </p>
         {restaurant.link ?
         <p className="card-text">link: {restaurant.link}</p>
         :null
         }
-        {restaurant.meals ?
-        <p className="card-text">Meals {restaurant.meals}</p>
+        {restaurant.meals.length ?
+        <p className="card-text">Meals {restaurant.meals.length}</p>
         :null
         }
       </div>
-      {loggedIn &&
-        user.profile === restaurant.creator?._id ?
-          <div className="card-footer">
-             <Link
-              className='btn btn-sm btn-primary'
-              to='/profiles/profile/schedule'
-            >
-              Add location
-            </Link>
-            <Link
-              className='btn btn-sm btn-warning'
-              to='/editRestaurant'
-              state={{restaurant}}
-            >
-              Edit
-            </Link>
-            <button
-              className="btn btn-sm btn-danger m-left"
-              onClick={()=> handleDeleteRestaurant(restaurant._id)}
-            >
-              Delete
-            </button>
-          </div>
-        :
-        <div className="card-footer">
-          <p className="card-text"> {restaurant.creator?.name ? restaurant.creator?.name : 'Chef'}'s Restaurant</p>
-        </div>
+      {location.state?.recipe ? 
+          <button
+          className="btn btn-sm btn-primary"
+          type="submit"
+        >
+          Add this Restaurant
+        </button>
+          :
+          loggedIn &&
+            user.profile === restaurant.creator?._id ?
+              <div className="card-footer">
+                <Link
+                  className='btn btn-sm btn-primary'
+                  to='/profiles/profile/schedule'
+                >
+                  Add location
+                </Link>
+                <Link
+                  className='btn btn-sm btn-warning'
+                  to='/editRestaurant'
+                  state={{restaurant}}
+                >
+                  Edit
+                </Link>
+                <button
+                  className="btn btn-sm btn-danger m-left"
+                  onClick={()=> handleDeleteRestaurant(restaurant._id)}
+                >
+                  Delete
+                </button>
+              </div>
+            :
+            <div className="card-footer">
+              <p className="card-text"> {restaurant.creator?.name ? restaurant.creator?.name : 'Chef'}'s Restaurant</p>
+            </div>
       }
       
     </div>
